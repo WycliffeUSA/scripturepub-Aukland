81f661afaf6b2dc6c3465ca5f08e781eec490129
esmond <esmond-leung@hotmail.com>
2016-11-05 22:00:57 +1300

    Autofac di added

---
+++ ScripturePublishing/AutofacConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+namespace ScripturePublishing
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterFilterProvider();
+
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            builder.RegisterModule(new AutofacModule());
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
---
+++ ScripturePublishing/AutofacModule.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using Autofac;
+using ScripturePublishingEntity;
+using ScripturePublishingEntity.Repositories;
+
+namespace ScripturePublishing
+{
+    public class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            builder.RegisterType<WorkflowEngine>().WithParameter("connection", connectionString).InstancePerRequest();
+           // builder.RegisterType<UnitOfWork<ApprovalServiceDbContext>>().As<IUnitOfWork>().InstancePerRequest();
+
+            //Repositories
+            builder.RegisterType<ProcessRepository>().As<IProcessRepository>().InstancePerRequest();
+
+            //Services
+            //builder.RegisterType<ScratchPadService>().As<IScratchPadService>().InstancePerRequest();
+
+            base.Load(builder);
+        }
+    }
+}
